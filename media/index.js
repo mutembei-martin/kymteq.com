5fdff39548f8c82af86510468e5ba18d76c9eb88
mutembei-martin <walkerrigby@gmail.com>
2018-10-25 20:24:27 +0100

    start

---
+++ media/index.js
@@ -0,0 +1,253 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+var TextScramble = function () {
+  function TextScramble(el) {
+    _classCallCheck(this, TextScramble);
+
+    this.el = el;
+    this.chars = '!<>-_\\/[]{}—=+*^?#________';
+    this.update = this.update.bind(this);
+  }
+
+  TextScramble.prototype.setText = function setText(newText) {
+    var _this = this;
+
+    var oldText = this.el.innerText;
+    var length = Math.max(oldText.length, newText.length);
+    var promise = new Promise(function (resolve) {
+      return _this.resolve = resolve;
+    });
+    this.queue = [];
+    for (var i = 0; i < length; i++) {
+      var from = oldText[i] || '';
+      var to = newText[i] || '';
+      var start = Math.floor(Math.random() * 40);
+      var end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from: from, to: to, start: start, end: end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  };
+
+  TextScramble.prototype.update = function update() {
+    var output = '';
+    var complete = 0;
+    for (var i = 0, n = this.queue.length; i < n; i++) {
+      var _queue$i = this.queue[i];
+      var from = _queue$i.from;
+      var to = _queue$i.to;
+      var start = _queue$i.start;
+      var end = _queue$i.end;
+      var char = _queue$i.char;
+
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += '<span class="dud">' + char + '</span>';
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  };
+
+  TextScramble.prototype.randomChar = function randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  };
+
+  return TextScramble;
+}();
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+var phrases = ['User Interface.', 'User Experience.', 'Mobile Applications.', 'Brand Design.', 'Art Explosion.'];
+
+var el = document.querySelector('.realize');
+var fx = new TextScramble(el);
+
+var counter = 0;
+var next = function next() {
+  fx.setText(phrases[counter]).then(function () {
+    setTimeout(next, 800);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
+
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2*Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, 'white');
+  grad.addColorStop(1, '#333');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius*0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius*0.15 + "px arial";
+  ctx.textBaseline="middle";
+  ctx.textAlign="center";
+  for(num = 1; num < 13; num++){
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius*0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius*0.85);
+    ctx.rotate(-ang);
+  }
+}
+
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour=hour%12;
+    hour=(hour*Math.PI/6)+
+    (minute*Math.PI/(6*60))+
+    (second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
+
+var month;
+switch (new Date().getMonth()) {
+    case 0:
+        day = "JAN";
+        break;
+    case 1:
+        day = "FEB";
+        break;
+    case 2:
+        day = "MAR";
+        break;
+    case 3:
+        day = "APR";
+        break;
+    case 4:
+        day = "MAY";
+        break;
+    case 5:
+        day = "JUN";
+        break;
+    case  6:
+        day = "JUL";
+    case 7:
+        day = "AUG";
+        break;
+    case 8:
+        day = "SEP";
+        break;
+    case 9:
+        day = "OCT";
+        break;
+    case 10:
+        day = "NOV";
+        break;
+    case 11:
+        day = "DEC";
+        
+}
+
+document.getElementById("mon").innerHTML =day;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var d = new Date();
+document.getElementById("year").innerHTML=d.getFullYear();
+
+//////////////////////////////////////////////////////////////    DAY      ////////////////////////////////////////////////////////////////////
+
+var day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "SUN";
+        break;
+    case 1:
+        day = "MON";
+        break;
+    case 2:
+        day = "TUE";
+        break;
+    case 3:
+        day = "WED";
+        break;
+    case 4:
+        day = "THU";
+        break;
+    case 5:
+        day = "FRI";
+        break;
+    case  6:
+        day = "SAT";
+}
+
+document.getElementById("day").innerHTML =day;
+var d = new Date();
+document.getElementById("date").innerHTML=d.getDate()+ "th";
